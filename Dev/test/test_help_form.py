807d2882ccdb05f40e04aa85260b0410df9844dc
Anish-shrestha10 <anish.shrestha7077@gmail.com>
2025-11-25 07:53:22 +0545

    little change

---
+++ Dev/test/test_help_form.py
@@ -0,0 +1,55 @@
+from playwright.sync_api import Playwright
+
+from Dev.Page.Help_form import HelpForm
+
+
+def test_help(playwright:Playwright):
+    obj = HelpForm(playwright)
+    obj.navigate(
+        {
+            "email":"qa.site1.1@gmail.com",
+            "password":"Password@123"
+        })
+    obj.form(
+        {
+            "fullname":"Anish shrestha",
+            "country_code":"Nepal (+977)",
+            "number":"9843125788",
+            "email":"anish@gmail.com",
+            "role":"Sponsor",
+            "message":"hello world"
+        })
+
+def test_invalid_data(playwright:Playwright):
+    obj = HelpForm(playwright)
+    obj.navigate(
+        {
+            "email":"qa.site1.1@gmail.com",
+            "password":"Password@123"
+        })
+    obj.form(
+        {
+            "fullname":"Anish shrestha",
+            "country_code":"Nepal (+977)",
+            "number":"98431258",
+            "email":"anish@gmail.com",
+            "role":"Sponsor",
+            "message":"hello world"
+        })
+
+def test_invalid_email(playwright:Playwright):
+    obj = HelpForm(playwright)
+    obj.navigate(
+        {
+            "email":"qa.site1.1@gmail.com",
+            "password":"Password@123"
+        })
+    obj.form(
+        {
+            "fullname":"Anish shrestha",
+            "country_code":"Nepal (+977)",
+            "number":"98431258",
+            "email":"anish@gma",
+            "role":"Sponsor",
+            "message":"hello world"
+        })
